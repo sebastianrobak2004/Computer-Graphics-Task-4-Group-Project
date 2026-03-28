ec7f63ba13c10de68a65323cf4a0fd3ec41f7438
Matt <matthew@adler.id.au>
2026-03-28 16:13:28 +1100

    Task: Fix main in reference to controls.js changes

--- src/main.js
+++ src/main.js
@@ -1,56 +1,45 @@
 import * as THREE from 'three';
 import { scene, camera, renderer } from './scene_manager.js';
 import { floor_mesh } from './floor.js';
-import { camera_controls } from './controls.js';
-
+import { movementState } from './controls.js';
 
 scene.add(floor_mesh);
 
-camera.position.set(0,0,-10);
-camera.lookAt(0,-10,1);
-
+camera.position.set(0, 0, -10);
+camera.lookAt(0, -10, 1);
 
 function animate() {
-    requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 
-    
-    renderer.render(scene, camera);
-};
+	renderer.render(scene, camera);
+}
 
 function move_camera() {
-    if(camera_controls.foward){
-        console.log("foward");
-    };
+	if (movementState.isMovingForward) {
+		console.log('forward');
+	}
 
-    if(camera_controls.backward){
-        console.log("b");
-    };
+	if (movementState.isMovingBackward) {
+		console.log('backward');
+	}
 
-    if(camera_controls.left){
+	if (camera_controls.isMovingLeft) {
+	}
 
-    };
+	if (camera_controls.isMovingRight) {
+	}
 
-    if(camera_controls.right){
-        
-    };
+	if (camera_controls.isRising) {
+	}
 
-    if(camera_controls.rise){
+	if (camera_controls.isFalling) {
+	}
 
-    };
+	if (camera_controls.isTurningRight) {
+	}
 
-    if(camera_controls.fall){
-
-    };
-
-    if(camera_controls.look_right){
-
-    };
-
-    if(camera_controls.look_left){
-
-    };
-    
-};
+	if (camera_controls.isTurningLeft) {
+	}
+}
 
 animate();
-
