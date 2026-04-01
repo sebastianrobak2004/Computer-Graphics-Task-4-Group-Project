449e5e8351b7f2df9ab0040cf99af7e08ce84ff1
sebastian robak <sebastian.p.robak@student.uts.edu.au>
2026-04-01 18:33:52 +1100

    reduced gaps between rings

--- src/levelGenerator.js
+++ src/levelGenerator.js
@@ -2,20 +2,24 @@ import * as THREE from 'three';
 import { registerFunctionForSetup, scene, timer } from './sceneManager.js';
 
 const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
-const epsilon = 0.01;
-const enableLogs = true;
+
+const epsilon = 1;
+const enableLogs = false;
 
 export const startLevelGeneration = () => {
     const level = new THREE.Group();
     const ring = createRingMesh();
     const scaleVec = new THREE.Vector3;
     
-    const MAX_RINGS = 16;
-    const SPEED = 2;
+    const MAX_RINGS = 10;
+    const MAX_SCALE = 1024;
+    const SPEED = 1;
+
     let derivedRadius = 1;
 
     ring.scale.set(epsilon,epsilon,1);
     level.scale.set(1,1,1);
+    level.position.set(0,0,0);
     
     level.rotateX(Math.PI/2);
     level.position.y -= 35;
@@ -32,56 +36,67 @@ export const startLevelGeneration = () => {
             const cur = ring.clone();
             const proceduralList = generateProceduralRing(8);
 
-            const segments = [...cur.children]; // remove segments using procedural bool list
+            const segments = [...cur.children];
             segments.forEach((segment, i) => {
-                if (proceduralList[i]) {
+                if (!proceduralList[i]) {
                     cur.remove(segment);
                 }
             });
-            const overshoot = derivedRadius - 1;
-            //log("overshoot = ", overshoot)
+            const adjustment = 2 / derivedRadius;
+
 
             level.add(cur);
-            cur.scale.set((epsilon * overshoot)/level.scale.x, (epsilon * overshoot)/level.scale.y , 1);
+            const inverseScale = 1 / level.scale.x;
+            cur.scale.x = (epsilon * adjustment * inverseScale);
+            cur.scale.y = (epsilon * adjustment * inverseScale);
+            
 
-            derivedRadius = 1;
+            derivedRadius -= 1;
             if (level.children.length > MAX_RINGS ){
                 level.remove(level.children[0]);
             }
         }
         scaleVec.set(multiplier, multiplier, 1);
         level.scale.multiply(scaleVec);
-        //log("Length of rings", level.children.length );
-        //log("length of Scene.Children", scene.children.length);
+        
+        if ( level.scale.x > MAX_SCALE ){
+            const scaleDown = level.scale.x;
+
+            level.children.forEach( child => {
+                child.scale.multiply(new THREE.Vector3(scaleDown, scaleDown, 1));
+            });
+
+            level.scale.set(1 ,1 ,1 );
+
+
+        }
     }
 
     registerFunctionForSetup(levelUpdate);
 }
 
 
-function generateProceduralRing(divisions){
-    const list = Array.from({ length: divisions }, () => Math.random() < 0.5);
+const generateProceduralRing = (divisions) => {
+    const list = Array.from({ length: divisions }, () => Math.random() < 0.30);
+    //const list = [true,true,true,false,false,false,false,false];
     return list
 }
 
 
-function createRingMesh({
-    divisions = 8,
-
-} = {} ){
-    
-
+const createRingMesh = (divisions = 8 ) => {
     const angleIncrement = (2 * Math.PI) / divisions;
     const segmentGeometrys = [];
     
 
     for (let i = 1; i <= divisions; i++) {
-
-        segmentGeometrys.push( createDonutSlice({startAngle: angleIncrement * i, endAngle: angleIncrement * ( i + 1 )}) );
+        segmentGeometrys.push(
+            createDonutSlice({
+                startAngle: angleIncrement * i, 
+                endAngle: angleIncrement * ( i + 1 )
+            }));
     }
 
     const ring = new THREE.Group();
-    
     segmentGeometrys.forEach( segment => {
         const mesh = new THREE.Mesh(segment, mat);
         ring.add(mesh);
@@ -91,15 +106,15 @@ function createRingMesh({
     return ring;
 };
 
-function createDonutSlice({
+const createDonutSlice = ({
     innerRadius = 1,
     outerRadius = 2,
     startAngle = 0,
     endAngle = Math.PI / 2,
-    segments = 4,
+    segments = 100,
     depth = 0.5,          
     bevelEnabled = false
-} = {}) {
+} = {}) => {
     const shape = new THREE.Shape();
 
     shape.absarc(0, 0, outerRadius, startAngle, endAngle, false, segments);
@@ -124,11 +139,3 @@ function createDonutSlice({
 
     return geometry;
 }
-
-function log(string, value){
-    if (!enableLogs) return;
-
-        console.log(`${string} : ${value}`);
-}
-    
-
