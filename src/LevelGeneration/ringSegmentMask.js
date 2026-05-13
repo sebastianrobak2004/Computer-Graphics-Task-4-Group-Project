28a7afd920e15ad0db859e5fed015b0c027b2015
sebastianrobak2004 <sebastian.p.robak@student.uts.edu.au>
2026-05-14 01:53:32 +1000

    Decent procedural level generation

--- src/LevelGeneration/ringSegmentMask.js
+++ src/LevelGeneration/ringSegmentMask.js
@@ -1,16 +1,53 @@
 
-const segments = 8;
-const lastRing = [];
+let segmentsNum = 8;
+let lastR = [];
 
 export const generateMask = (segments, lastRing) => {
-    segments = segments;
-    lastRing = lastRing;
+    segmentsNum = segments;
+    lastR = lastRing;
 
-    return randomMask(0.5);
+    return proceduralMask();
 }
 
 const randomMask = (chance) => Array.from({ length: segments }, () => Math.random() < chance);
 
+const mod = (n, m) => ((n % m) + m) % m;
+const randBool = () => Math.random() < 0.5 ? true : false;
+
 const proceduralMask = () => {
-    return [];
+    if(lastR == null){
+        lastR = [false,false,false,false,false,false,true,true];
+        console.log(lastR);
+    }
+
+    let nextR = [false,false,false,false,false,false,false,false];
+
+    let first = true;
+    //const dir = Math.random() < 0.5 ? 1 : -1;
+
+    for (let i = 1; i < lastR.length; i += 1){
+        const cur = lastR[i];
+        const prev = lastR[mod(i + 1, lastR.length)];
+        const next = lastR[mod(i - 1, lastR.length)];
+
+        if(!cur){
+            if(!first) first = true;
+            continue;
+        }
+
+        if(first){
+            nextR[i] = true;
+            first = false;
+        }else{
+            nextR[i] = randBool();
+        }
+
+        if(nextR[i]){
+            if(!prev) nextR[mod(i + 1, lastR.length)] = randBool();
+            if(!next) nextR[mod(i - 1, lastR.length)] = randBool();
+        }
+    }
+    
+    console.log(nextR);
+    return nextR;
 }
